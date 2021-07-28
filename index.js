6a523c5dd028aad75bc314e155916272b5dce33e
SANJAY-KUMAR-7 <83998914+SANJAY-KUMAR-7@users.noreply.github.com>
2021-07-28 16:02:34 +0530

    Date:28 July,2021

---
+++ index.js
@@ -0,0 +1,19 @@
+ var randomNumber1;
+randomNumber1=Math.floor(Math.random()*6)+1;
+//console.log(randomNumber1);
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+var randomNumber2;
+randomNumber2=Math.floor(Math.random()*6)+1;
+//console.log(randomNumber1);
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML=("Player 1 Wins!");
+}
+else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML=("Player 2 Wins!"); 
+}
+else{
+    document.querySelector("h1").innerHTML=("Draw!");
+}
